67b39f1d1a5d39477a19e5924e98c295e3e190cf
Anu <anukriti.verma2019@vitstudent.ac.in>
2023-03-22 19:21:37 +0530

    header icons

--- docstream/components/modal/DownloadHelloSign.js
+++ docstream/components/modal/DownloadHelloSign.js
@@ -15,21 +15,21 @@ function DownloadHelloSign(props) {
     const buttonRef = useRef();
     const [items, setItems] = React.useState([]);
 
-    async function handleClick(e){
+    async function handleClick(e) {
         setShowModal(true);
         $.ajax({
             type: "GET",
             url: " http://localhost:3003/getPendingList",
             headers: {
                 "accept": "application/json",
-                "Access-Control-Allow-Origin":"*"
+                "Access-Control-Allow-Origin": "*"
             }
-          }).done(function (o) {
+        }).done(function (o) {
             setItems(o);
-          });
+        });
 
     }
-    
+
     let itemList = [];
     items.forEach((item, index) => {
         itemList.push(<DownloadCard Name={item.title} Title={item.subject} Url={item.files_url} />)
@@ -37,20 +37,16 @@ function DownloadHelloSign(props) {
 
     return (
         <>
-            <Button
-                color="lightBlue"
-                buttonType="filled"
-                size="regular"
-                className="md:inline-flex h-10 mx-12"
-                rounded={false}
-                block={false}
-                iconOnly={false}
-                ripple="light"
-                onClick={handleClick}
-            >
-                <Icon name="people" size="md" />
-                {props.text}
-            </Button>
+            <div onClick={handleClick} className='button'>
+                <svg width="34" height="39" viewBox="0 0 34 39" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20.8538 31.9779C20.7167 28.4763 23.5393 25.7828 27.0211 25.7828C30.5029 25.7828 33.3255 28.5564 33.3255 31.9779C33.3255 35.3994 30.5029 38.173 27.0211 38.173C27.0211 38.173 24.2753 38.3077 22.3566 35.7488M20.8538 31.9779L19.1476 30.852M20.8538 31.9779L22.5503 30.852" stroke="#4CAF50" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M26.6052 28.7456V31.9779L29.0721 32.9206" stroke="#4CAF50" stroke-linecap="round" />
+                    <path d="M22.6457 1H1V35.4564H20.849M22.6457 1V5.76525H26.8352M22.6457 1L26.8352 5.76525M26.8352 5.76525V24.6564" stroke="#4CAF50" />
+                    <path d="M6.67114 31.0892C7.79829 30.2616 8.95224 28.1414 8.53093 26.3368C8.02372 24.1643 6.25911 29.0821 8.9396 29.0822C9.50671 29.0822 9.27485 30.3443 8.86907 31.0892C8.36186 32.0203 9.71443 29.9059 10.2216 29.9059C10.7437 29.9059 10.8171 31.7439 10.1936 31.4577C9.51733 31.1474 12.6205 30.2163 11.7752 29.9059C11.0989 29.9059 11.2402 31.2284 11.7752 31.3026C12.195 31.3607 14.0436 30.852 14.0436 31.4577" stroke="#4CAF50" stroke-width="0.5" stroke-linecap="round" />
+                    <path d="M4.40265 5.76526H9.56968M4.40265 8.32228H9.56968M8.37244 13.9548H19.7147M4.96976 16.771H21.9832M4.96976 19.5872H21.9832M4.96976 22.4035H15.7449" stroke="#4CAF50" stroke-linecap="round" />
+                </svg>
+
+            </div>
 
             <Modal size="lg" active={showModal} toggler={() => setShowModal(false)}>
 
--- docstream/components/modal/HelloSign.js
+++ docstream/components/modal/HelloSign.js
@@ -49,20 +49,15 @@ function HelloSign(props) {
 
   return (
     <>
-      <Button
-        color="lightBlue"
-        buttonType="filled"
-        size="regular"
-        className="md:inline-flex h-10 mx-12"
-        rounded={false}
-        block={false}
-        iconOnly={false}
-        ripple="light"
-        onClick={(e) => setShowModal(true)}
-      >
-        <Icon name="people" size="md" />
-        {props.text}
-      </Button>
+      <div onClick={(e) => setShowModal(true)} className="button">
+        <svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M20.5956 27.8091L30.7744 11.0066L34.7525 13.4106L24.5737 30.2131L20.5489 32.3716L20.5956 27.8091Z" stroke="#4CAF50" />
+          <path d="M35.9957 11.5487L31.9273 9.22672L32.6165 7.85006C32.8735 7.33673 33.5093 7.14427 34.0079 7.42886L36.3365 8.75793C36.7974 9.02101 36.9722 9.59809 36.7346 10.0726L35.9957 11.5487Z" stroke="#4CAF50" />
+          <path d="M23.1831 1H1V36.511H27.4766V25.3804M23.1831 1V5.91109H27.4766M23.1831 1L27.4766 5.91109M27.4766 5.91109V16.342" stroke="#4CAF50" />
+          <path d="M6.81195 32.0102C7.96708 31.1572 9.14968 28.9721 8.71792 27.1123C8.19811 24.8733 6.38968 29.9416 9.13673 29.9417C9.71792 29.9418 9.4803 31.2425 9.06446 32.0102C8.54465 32.9698 9.9308 30.7907 10.4506 30.7907C10.9857 30.7907 11.0609 32.6849 10.4219 32.39C9.72881 32.0701 12.909 31.1105 12.0427 30.7907C11.3496 30.7906 11.4944 32.1537 12.0427 32.23C12.473 32.29 14.3675 31.7657 14.3675 32.39" stroke="#4CAF50" stroke-width="0.5" stroke-linecap="round" />
+          <path d="M4.48712 5.91107H9.78245M4.48712 8.54636H9.78245M8.55548 14.3512H20.1794M5.06832 17.2536H22.5042M5.06832 20.1561H22.5042M5.06832 23.0585H16.111" stroke="#4CAF50" stroke-linecap="round" />
+        </svg>
+      </div>
 
       <Modal size="lg" active={showModal} toggler={() => setShowModal(false)}>
 
